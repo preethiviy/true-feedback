552e352dfc6013998e7692cae3e4cdbaf7334be2
preethiviy <preethivirajyambem@gmail.com>
2024-07-11 20:23:49 +0530

    shadecn added

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,4 +1,11 @@
-import React from 'react'
+'use client'
+
+import React from 'react';
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import Link from 'next/link';
+import { useDebounceValue } from 'usehooks-ts'
 
 const page = () => {
   return (
