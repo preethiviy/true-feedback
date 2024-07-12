18c4eaad3909fd7676dcb909af63189d127db391
preethiviy <preethivirajyambem@gmail.com>
2024-07-12 13:57:21 +0530

    sign-up ui

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,13 +1,7 @@
-'use client'
-
 import React from 'react';
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import Link from 'next/link';
-import { useDebounceValue } from 'usehooks-ts'
 
 const page = () => {
+  
   return (
     <div>page</div>
   )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <AuthProvider>
-        <body className={inter.className}>{children}</body>
-        <Toaster />
+        <body className={inter.className}>
+          {children}
+          <Toaster />
+        </body>
       </AuthProvider>
     </html>
   );
